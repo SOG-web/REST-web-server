515b437565763cdd2ca1675708de0ddfac2c2a3a
SOG-web <raheemolalekanusman84@gmail.com>
2020-12-03 13:12:18 +0100

    mongodb created and books added

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 require('dotenv').config();
 const express = require('express');
+const debug = require('debug')('app');
+const chalk = require('chalk');
+const mongoose = require('mongoose');
 
 const app = express();
+const bookRouter = express.Router();
 
 // Read the host address and the port from the environment
 const hostname = process.env.HOST;
 const port = process.env.PORT;
 
+bookRouter.route('/books')
+  .get((req, res) => {
+    const response = { hello: 'This is my API' };
+    res.json(response);
+  });
+
+app.use('/api', bookRouter);
 app.get('/', (req, res) => {
   res.send('Welcome to my API!');
 });
 
 app.listen(port, () => {
-  console.log(`Running on ${hostname}:${port}`);
+  debug(`server listing on ${chalk.bgBlue.bold.black(hostname)}:${chalk.green(port)}`);
 });
